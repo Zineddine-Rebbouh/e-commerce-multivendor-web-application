4ce0a1dfb0f543e26505fd12cb5609b95386bfc2
Zineddine-Rebbouh <119822151+Zineddine-Rebbouh@users.noreply.github.com>
2026-08-10 22:02:55 -1000

    applied many updates : fix bugs add improvements ready for deploy

--- Frontend/src/components/CheckOut/Checkout.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Stripe from "stripe";
-
-export const Checkout = () => {
-  return <div>Checkout</div>;
-};
--- README.md
+++
@@ -1,3 +0,0 @@
-# e-commerce multi-seller website
-
-This project is for my final Bachelor of Computer Science project.
